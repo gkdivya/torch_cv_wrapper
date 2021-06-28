eded34fcb14c00c5b52b0440a7608e7a66c8a56c
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-06-28 17:10:26 +0530

    Update albumentation.py

--- dataloader/albumentation.py
+++ dataloader/albumentation.py
@@ -2,25 +2,28 @@ import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 import numpy as np
 from utils import helper
+import torchvision.transforms as transforms
 
-def data_albumentations(horizontalflip_prob,rotate_limit,shiftscalerotate_prob,num_holes,cutout_prob):
-    # Calculate mean and std deviation for cifar dataset
-    mean,std = helper.calculate_mean_std("CIFAR10")
+class CIFAR10Albumentation:
     
-    # Train Phase transformations
-    train_transforms = A.Compose([A.HorizontalFlip(p=horizontalflip_prob),
-                                  A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=rotate_limit, p=shiftscalerotate_prob),
-                                  A.CoarseDropout(max_holes=num_holes,min_holes = 1, max_height=16, max_width=16, 
-                                  p=cutout_prob,fill_value=tuple([x * 255.0 for x in mean]),
-                                  min_height=16, min_width=16),
-                                  A.ColorJitter(p=0.25,brightness=0.3, contrast=0.3, saturation=0.30, hue=0.2),
-                                  A.ToGray(p=0.15),
-                                  A.Normalize(mean=mean, std=std,always_apply=True),
-                                  ToTensorV2()
-                                ])
-
-    # Test Phase transformations
-    test_transforms = A.Compose([A.Normalize(mean=mean, std=std, always_apply=True),
+    def __init__(self):
+        pass
+    
+    def train_transform(self,mean,std):
+        # Train Phase transformations
+        train_transforms = A.Compose([A.PadIfNeeded(min_height=40, min_width=40, always_apply=True),
+                                      A.RandomCrop(width=32, height=32,p=1),
+                                      A.Rotate(limit=5),
+                                      #A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=5, p=0.25),
+                                      A.CoarseDropout(max_holes=1,min_holes = 1, max_height=16, max_width=16, p=0.5,fill_value=tuple([x * 255.0 for x in mean]),
+                                      min_height=16, min_width=16),
+                                      A.Normalize(mean=mean, std=std,always_apply=True),
+                                      ToTensorV2()
+                                    ])
+        return lambda img:train_transforms(image=np.array(img))["image"]
+                                
+    def test_transform(self,mean,std):
+        # Test Phase transformations
+        test_transforms = A.Compose([A.Normalize(mean=mean, std=std, always_apply=True),
                                  ToTensorV2()])
-
-    return lambda img:train_transforms(image=np.array(img))["image"],lambda img:test_transforms(image=np.array(img))["image"]
+        return lambda img:test_transforms(image=np.array(img))["image"]
