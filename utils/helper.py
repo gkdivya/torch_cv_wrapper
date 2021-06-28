283df8a5b4b7ab98ec15ba0bcc71c15cf0ccbef1
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-06-28 17:34:03 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,7 +1,5 @@
 import torch
 import matplotlib.pyplot as plt
-from model import model as m
-
 from torchsummary import summary
 import yaml
 from pprint import pprint
@@ -9,8 +7,29 @@ import random
 import numpy as np
 import torch.nn as nn
 from torchvision import datasets, transforms
+from itertools import product
 
 
+
+def imshow(img):
+    # functions to show an image
+    fig, ax = plt.subplots(figsize=(12, 12))
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    
+def unnormalize(img):
+  mean = (0.49139968, 0.48215841, 0.44653091)
+  std = (0.24703223, 0.24348513, 0.26158784)
+#   mean,std = calculate_mean_std("CIFAR")
+  img = img.cpu().numpy().astype(dtype=np.float32)
+  
+  for i in range(img.shape[0]):
+    img[i] = (img[i]*std[i])+mean[i]
+  
+  return np.transpose(img, (1,2,0))
+  
+  
 def calculate_mean_std(dataset):
     if dataset == 'CIFAR10':
         train_transform = transforms.Compose([transforms.ToTensor()])
@@ -63,3 +82,35 @@ def class_level_accuracy(model, loader, device, classes):
 
     for i in range(10):
         print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
+
+def compute_confusion_matrix(model, data_loader, device):
+
+    all_targets, all_predictions = [], []
+    with torch.no_grad():
+
+        for i, (features, targets) in enumerate(data_loader):
+
+            features = features.to(device)
+            targets = targets
+            logits = model(features)
+            _, predicted_labels = torch.max(logits, 1)
+            all_targets.extend(targets.to('cpu'))
+            all_predictions.extend(predicted_labels.to('cpu'))
+
+    all_predictions = all_predictions
+    all_predictions = np.array(all_predictions)
+    all_targets = np.array(all_targets)
+        
+    class_labels = np.unique(np.concatenate((all_targets, all_predictions)))
+    if class_labels.shape[0] == 1:
+        if class_labels[0] != 0:
+            class_labels = np.array([0, class_labels[0]])
+        else:
+            class_labels = np.array([class_labels[0], 1])
+    n_labels = class_labels.shape[0]
+    lst = []
+    z = list(zip(all_targets, all_predictions))
+    for combi in product(class_labels, repeat=2):
+        lst.append(z.count(combi))
+    mat = np.asarray(lst)[:, None].reshape(n_labels, n_labels)
+    return mat
