c386564db3b03f524e53df69accc8d4284de6742
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-06-30 05:54:07 +0530

    Update plot_metrics.py

--- utils/plot_metrics.py
+++ utils/plot_metrics.py
@@ -4,29 +4,32 @@ import numpy as np
 
 def plot_metrics(exp_metrics):
     sns.set(font_scale=1)
+    fig, axs = plt.subplots(2,2,figsize=(25,15))
     plt.rcParams["figure.figsize"] = (25,6)
     train_accuracy,train_losses,test_accuracy,test_losses  = exp_metrics
     
-    # Plot the learning curve.
-    fig, (ax1,ax2) = plt.subplots(1,2)
-    ax1.plot(np.array(test_losses), 'b', label="Validation Loss")
     
-    # Label the plot.
-    ax1.set_title("Validation Loss")
-    ax1.set_xlabel("Epoch")
-    ax1.set_ylabel("Loss")
-    ax1.legend()
-    
-    ax2.plot(np.array(test_accuracy), 'b', label="Validation Accuracy")
-    
-    # Label the plot.
-    ax2.set_title("Validation Accuracy")
-    ax2.set_xlabel("Epoch")
-    ax2.set_ylabel("Loss")
-    ax2.legend()
-    
-    plt.show()
-    
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].set_title("Test Accuracy")
+
+    axs[0, 0].plot(train_losses, label="Training Loss")
+    axs[0,0].set_xlabel('epochs')
+    axs[0,0].set_ylabel('loss')
+
+    axs[1, 0].plot(train_accuracy, label="Training Accuracy")
+    axs[1,0].set_xlabel('epochs')
+    axs[1,0].set_ylabel('accuracy')
+
+    axs[0, 1].plot(test_losses, label="Validation Loss")
+    axs[0,1].set_xlabel('epochs')
+    axs[0,1].set_ylabel('loss')
+
+    axs[1, 1].plot(test_accuracy, label="Validation Accuracy")
+    axs[1,1].set_xlabel('epochs')
+    axs[1,1].set_ylabel('accuracy')
+        
     
 def plot_confusion_matrix(conf_mat,
                           hide_spines=False,
