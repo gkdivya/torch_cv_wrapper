7129efaa6ba0d48d595c9cbb7fe81fee9f3568e9
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-07-15 16:40:58 +0530

    Update albumentation.py

--- dataloader/albumentation.py
+++ dataloader/albumentation.py
@@ -51,3 +51,27 @@ class CIFAR10AlbumentationS9:
         test_transforms = A.Compose([A.Normalize(mean=mean, std=std, always_apply=True),
                                  ToTensorV2()])
         return lambda img:test_transforms(image=np.array(img))["image"]
+        
+        
+class TinyImageNetAlbumentation:
+    
+    def __init__(self):
+        pass
+    
+    def train_transform(self,mean,std):
+        # Train Phase transformations
+        train_transforms = A.Compose([  A.PadIfNeeded(min_height=70, min_width=70, always_apply=True),
+                                        A.RandomCrop(height=64, width=64,p=1),
+                                        A.HorizontalFlip(p=0.5),
+                                        A.CoarseDropout(max_holes=1,min_holes = 1, max_height=32, max_width=32, p=0.8,fill_value=tuple([x * 255.0 for x in mean]),
+                                      min_height=8, min_width=8),
+                                        A.Normalize(mean=mean, std=std,always_apply=True),
+                                        ToTensorV2()
+                                    ])
+        return lambda img:train_transforms(image=np.array(img))["image"]
+                                
+    def test_transform(self,mean,std):
+        # Test Phase transformations
+        test_transforms = A.Compose([A.Normalize(mean=mean, std=std, always_apply=True),
+                                 ToTensorV2()])
+        return lambda img:test_transforms(image=np.array(img))["image"]
