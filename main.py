d0d645f6f11f196ff1dca9228ce874b73b1f1028
Divya Kamat <42609155+divya-r-kamat@users.noreply.github.com>
2021-07-16 06:13:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,184 +1,167 @@
-import torch
 import torchvision
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import torchvision.utils as utils
-from torch_cv_wrapper.dataloader.albumentation import *
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-from tqdm import notebook
-from PIL import Image
-import os
-import requests
-import zipfile
-from io import BytesIO
-import glob
-import csv
-import numpy as np
+from torch_cv_wrapper.dataloader.load_data import *
+import torch
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import StepLR,OneCycleLR,ReduceLROnPlateau
+from torch_cv_wrapper.utils import train as trn
+from torch_cv_wrapper.utils import test as tst
+from torchsummary import summary
+import yaml
+from pprint import pprint
 import random
+import numpy as np
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from torch_lr_finder import LRFinder
 
-class Cifar10DataLoader:
+
+class TriggerEngine:
     def __init__(self, config):
         self.config = config
-        self.augmentation = config['data_augmentation']['type']
-        
-    def calculate_mean_std(self):
-        train_transform = transforms.Compose([transforms.ToTensor()])
-        train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-        mean = train_set.data.mean(axis=(0,1,2))/255
-        std = train_set.data.std(axis=(0,1,2))/255
-        return mean, std
+        self.loader = config['data_loader']['type']
+        self.image_dataset=eval(self.loader)(self.config)
+        self.device = self.set_device()
+        self.writer = SummaryWriter()
+        self.l2_factor = self.config['training_params']['l2_factor']
 
-    def classes(self):
-        return ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'], None
         
-    def get_dataloader(self): 
+    def dataloader(self):
+        #Get dataloaders
+        return self.image_dataset.get_dataloader()
+       
+    def get_classes(self): 
+        return self.image_dataset.classes()
         
-        cifar_albumentation = eval(self.augmentation)()
-        mean,std = self.calculate_mean_std()
+    def set_device(self):
+        use_cuda = torch.cuda.is_available()
+        device = torch.device("cuda" if use_cuda else "cpu")
+        return device
         
-        train_transforms, test_transforms = cifar_albumentation.train_transform(mean,std),cifar_albumentation.test_transform(mean,std)
-                                                                              
-        trainset = datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=train_transforms)  
+    def run_experiment(self,model,train_loader,test_loader,lrmin=None,lrmax=None):
+        
+        model.to(self.device) 
+        dropout=self.config['model_params']['dropout']
+        epochs=self.config['training_params']['epochs']
+        
+        l1_factor = self.config['training_params']['l1_factor']
+        max_epoch = self.config['lr_finder']['max_epoch']
+        
+        criterion = nn.CrossEntropyLoss() if self.config['criterion'] == 'CrossEntropyLoss' else F.nll_loss()
+        opt_func = optim.Adam if self.config['optimizer']['type'] == 'optim.Adam' else optim.SGD
+        lr = self.config['optimizer']['args']['lr']
+        
+        grad_clip = 0.1
             
-        testset  = datasets.CIFAR10(root='./data', train=False,
-                                             transform=test_transforms)
-
-        self.train_loader = torch.utils.data.DataLoader(trainset, 
-                                                      batch_size=self.config['data_loader']['args']['batch_size'], 
-                                                      shuffle=True,
-                                                      num_workers=self.config['data_loader']['args']['num_workers'], 
-                                                      pin_memory=self.config['data_loader']['args']['pin_memory'])
-        self.test_loader = torch.utils.data.DataLoader(testset, 
-                                                     batch_size=self.config['data_loader']['args']['batch_size'],  
-                                                     shuffle=False,
-                                                     num_workers=self.config['data_loader']['args']['num_workers'], 
-                                                     pin_memory=self.config['data_loader']['args']['pin_memory'])
-        return self.train_loader,self.test_loader
-
-
-class TinyImageNetDataLoader:
-
-    def __init__(self, config=None):
-        self.config = config
-        if config is not None:
-            self.augmentation = config['data_augmentation']['type']
-        
-    def calculate_mean_std(self):
-        mean,std = (0.4802, 0.4481, 0.3975), (0.2302, 0.2265, 0.2262)
-        return mean, std
-
-    def classes(self):
-        id_dict = {}
-        all_classes = {}
-        for i, line in enumerate(open( 'data/tiny-imagenet-200/wnids.txt', 'r')):
-            id_dict[line.replace('\n', '')] = i
-        
-        result = {}
-        class_id={}
-        for i, line in enumerate(open( 'data/tiny-imagenet-200/words.txt', 'r')):
-            n_id, word = line.split('\t')[:2]
-            all_classes[n_id] = word
-        for key, value in id_dict.items():
-            result[value] = (all_classes[key].replace('\n','').split(",")[0])
-            class_id[key] = (value,all_classes[key])
+        train_losses = []
+        test_losses = []
+        train_accuracy = []
+        test_accuracy = []
+        plot_train_acc=[]
+        lrs=[]
             
-        return result,class_id  
         
-    def get_dataloader(self): 
         
-        tinyimagenet_albumentation = eval(self.augmentation)()
-        #cifar_albumentation = CIFAR10Albumentation()
-        mean,std = self.calculate_mean_std()
-        
-        train_transforms, test_transforms = tinyimagenet_albumentation.train_transform(mean,std),tinyimagenet_albumentation.test_transform(mean,std)
-                                                                              
-        trainset = TinyImageNet(root='./data', train=True,download=True, transform=train_transforms) 
-        # train_len = int(train_split*len(trainset))
-        # valid_len = len(trainset) - train_len
-        #imagenet_trainset, imagenet_valset = torch.utils.data.random_split(trainset, [train_len, valid_len])
+        if lrmax is not None:
+            optimizer = optim.SGD(model.parameters(), lr=lrmin, momentum=0.90,weight_decay=self.l2_factor)
+            if self.config['lr_scheduler'] == 'OneCycleLR': 
+                scheduler = OneCycleLR(optimizer=optimizer, max_lr=lrmax,
+                                      epochs=epochs, steps_per_epoch=len(train_loader),
+                                      pct_start=max_epoch/epochs,div_factor=8)
+            else:
+                scheduler = ReduceLROnPlateau(optimizer, factor=0.2, patience=3,verbose=True,mode='max')
+        else:
+            optimizer = opt_func(model.parameters(), lr=lr, momentum=0.90,weight_decay=self.l2_factor)
+            if self.config['lr_scheduler'] == 'OneCycleLR':
+                scheduler = OneCycleLR(optimizer, max_lr=lr,epochs=epochs,steps_per_epoch=len(train_loader))
+            else:
+                scheduler = ReduceLROnPlateau(optimizer, factor=0.2, patience=3,verbose=True,mode='max')
+
             
-        testset  = TinyImageNet(root='./data', train=False,transform=test_transforms)
+        for epoch in range(1, epochs + 1):
+            print(f'Epoch {epoch}:')
+            trn.train(model, self.device, train_loader, optimizer,epoch, train_accuracy, train_losses, l1_factor,scheduler,criterion,lrs,self.writer,grad_clip)
+            tst.test(model, self.device, test_loader,test_accuracy,test_losses,criterion)
+            
+            self.writer.add_scalar('Epoch/Train/train_loss', train_losses[-1], epoch)
+            self.writer.add_scalar('Epoch/Test/test_loss', test_losses[-1], epoch)
+            self.writer.add_scalar('Epoch/Train/train_accuracy', train_accuracy[-1], epoch)
+            self.writer.add_scalar('Epoch/Train/test_accuracy', test_accuracy[-1], epoch)
+            plot_train_acc.append(train_accuracy[-1])
 
-        self.train_loader = torch.utils.data.DataLoader(trainset, 
-                                                      batch_size=512, 
-                                                      shuffle=True,
-                                                      num_workers=2, 
-                                                      pin_memory=True)
-        self.test_loader = torch.utils.data.DataLoader(testset, 
-                                                     batch_size=512,  
-                                                     shuffle=False,
-                                                     num_workers=2, 
-                                                     pin_memory=True)
-        return self.train_loader,self.test_loader
+            if "ReduceLROnPlateau" in str(scheduler):
+                scheduler.step(test_accuracy[-1])
 
-
-class TinyImageNet(Dataset):
-    def __init__(self, root, train=True, transform=None,  download=False,train_split=0.7):
+            self.writer.flush()
+        return (plot_train_acc,train_losses,test_accuracy,test_losses)
+    
+    def find_lr(self,model,train_loader, test_loader, start_lr, end_lr):
         
-        self.root = root
-        self.transform = transform
-        self.data_dir = 'tiny-imagenet-200'
-
-        if download and (not os.path.isdir(os.path.join(self.root, self.data_dir))):
-            self.download()
-
-        self.image_paths = []
-        self.targets = []
-
-        _,class_id = TinyImageNetDataLoader().classes()
         
-        # train images
-        train_path = os.path.join(self.root, self.data_dir, 'train')
-        for class_dir in os.listdir(train_path):
-            train_images_path = os.path.join(train_path, class_dir, 'images')
-            for image in os.listdir(train_images_path):
-                if image.endswith('.JPEG'):
-                    self.image_paths.append(os.path.join(train_images_path, image))
-                    self.targets.append(class_id[class_dir][0])
+        lr_epochs = self.config['lr_finder']['lr_epochs']
+        num_iterations = len(test_loader) * lr_epochs
 
-        # val images
-        val_path = os.path.join(self.root, self.data_dir, 'val')
-        val_images_path = os.path.join(val_path, 'images')
-        with open(os.path.join(val_path, 'val_annotations.txt')) as f:
-            for line in csv.reader(f, delimiter='\t'):
-                self.image_paths.append(os.path.join(val_images_path, line[0]))
-                self.targets.append(class_id[line[1]][0])
-                
-        self.indices = np.arange(len(self.targets))
-
-        random_seed=1
-        np.random.seed(random_seed)
-        np.random.shuffle(self.indices)
-
-        split_idx = int(len(self.indices) * train_split)
-        self.indices = self.indices[:split_idx] if train else self.indices[split_idx:]
-
-    def download(self):
-        if (os.path.isdir("./data/tiny-imagenet-200")):
-            print('Images already downloaded...')
-            return
-        r = requests.get('http://cs231n.stanford.edu/tiny-imagenet-200.zip', stream=True)
-        print('Downloading TinyImageNet Data')
-        zip_ref = zipfile.ZipFile(BytesIO(r.content))
-        for file in notebook.tqdm(iterable=zip_ref.namelist(), total=len(zip_ref.namelist())):
-            zip_ref.extract(member=file, path='./data/')
-        zip_ref.close()
-
-
-    def __getitem__(self, index):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(model.parameters(), lr=start_lr, momentum=0.90, weight_decay=self.l2_factor)
+        lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
+        lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=end_lr, num_iter=num_iterations, step_mode="linear",diverge_th=50)
         
-        image_index = self.indices[index] 
-        filepath = self.image_paths[image_index]
-        img = Image.open(filepath)
-        img = img.convert("RGB")
-        target = self.targets[image_index]
+        # Plot
+        max_lr = lr_finder.history['lr'][lr_finder.history['loss'].index(lr_finder.best_loss)]
+        #max_lr = lr_finder.plot(suggest_lr=True,skip_start=0, skip_end=0)
 
-        if self.transform is not None:
-            img = self.transform(img)
+        # Reset graph
+        lr_finder.reset()
+        return max_lr
+    
+        
+    def save_experiment(self,model, experiment_name,path):
+        print(f"Saving the model for {experiment_name}")
+        torch.save(model, f'{path}/{experiment_name}.pt')
+    
+    def model_summary(self,model, input_size):
+        result = summary(model, input_size=input_size)
+        print(result)    
+        
+    def wrong_predictions(self,model,test_loader):
+        wrong_images=[]
+        wrong_label=[]
+        correct_label=[]
+        model.eval()
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(self.device), target.to(self.device)
+                output = model(data)        
+                pred = output.argmax(dim=1, keepdim=True).squeeze()  # get the index of the max log-probability
 
-        return img, target
-
-    def __len__(self):
-        return len(self.indices)
+                wrong_pred = (pred.eq(target.view_as(pred)) == False)
+                wrong_images.append(data[wrong_pred])
+                wrong_label.append(pred[wrong_pred])
+                correct_label.append(target.view_as(pred)[wrong_pred])  
+      
+                wrong_predictions = list(zip(torch.cat(wrong_images),torch.cat(wrong_label),torch.cat(correct_label)))    
+            print(f'Total wrong predictions are {len(wrong_predictions)}')
+            
+            self.plot_misclassified(wrong_predictions)
+      
+        return wrong_predictions
+        
+    def plot_misclassified(self,wrong_predictions):
+        fig = plt.figure(figsize=(15,12))
+        fig.tight_layout()
+        mean,std = self.image_dataset.calculate_mean_std()
+        for i, (img, pred, correct) in enumerate(wrong_predictions[:10]):
+            img, pred, target = img.cpu().numpy().astype(dtype=np.float32), pred.cpu(), correct.cpu()
+            for j in range(img.shape[0]):
+                img[j] = (img[j]*std[j])+mean[j]
+            
+            img = np.transpose(img, (1, 2, 0)) 
+            ax = fig.add_subplot(5, 5, i+1)
+            ax.axis('off')
+            self.class_names,_ = self.get_classes()
+            
+            ax.set_title(f'\nactual : {self.class_names[target.item()]}\npredicted : {self.class_names[pred.item()]}',fontsize=10)  
+            ax.imshow(img)  
+          
+        plt.show()
